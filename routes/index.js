6bfc40b2aa0b1868d84a0db272563acad4181d77
Kevin Exton <kcexton01@gmail.com>
2025-06-11 10:57:22 +0000

    Moved the routes aggregator to inside of the routes directory.

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+async function registerRoutes(app) {
+  const routesDir = __dirname; 
+  const routeFiles = fs.readdirSync(routesDir)
+    .filter(file => file.endsWith('.js') && file !== 'index.js');
+
+  await Promise.all(routeFiles.map(async (file) => {
+    const routePath = path.join(routesDir, file);
+    const route = require(routePath);
+    if (typeof route === 'function') {
+        await route(app);
+        app.log.info(`Registered ROUTES FROM ${path.relative(path.join(__dirname, '..'), routePath)}`);
+    }
+  }));
+}
+
+module.exports = registerRoutes;
